904c25c50040ceb921786cda71527e3b9f41d14e
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-06 08:55:36 +0530

    initial commit of java-spring-clean-architecture_candidate-recruitment-tracker-system

---
+++ src/main/java/lk/practice/candidate_recruitment_tacker/CandidateRecruitmentTackerApplication.java
@@ -0,0 +1,13 @@
+package lk.practice.candidate_recruitment_tacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateRecruitmentTackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandidateRecruitmentTackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/practice/candidate_recruitment_tacker/CandidateRecruitmentTackerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.practice.candidate_recruitment_tacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateRecruitmentTackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
