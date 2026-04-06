7177b8aed698b05f085a84fada41c9d65423dcc4
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-06 19:12:28 +0530

    feat: initiate usecase impl as bean by injecting domain repo

--- src/main/java/lk/practice/candidate_recruitment_tacker/usecase/CandidateUseCaseImpl.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/usecase/CandidateUseCaseImpl.java
@@ -1,4 +1,11 @@
 package lk.practice.candidate_recruitment_tacker.usecase;
 
-public class CandidateUseCaseImpl {
+import lk.practice.candidate_recruitment_tacker.domain.repository.CandidateRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CandidateUseCaseImpl implements CandidateUseCase{
+
+    //inject domain repo for initiate class as bean
+    private final CandidateRepository candidateRepository;
 }
