a0f029a1e21c4b7bf7893674a005210261a40b73
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-06 19:52:48 +0530

    feat: initiate table structure

--- src/main/java/lk/practice/candidate_recruitment_tacker/insfrastructure/persistence/CandidateEntity.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/insfrastructure/persistence/CandidateEntity.java
@@ -1,4 +1,30 @@
 package lk.practice.candidate_recruitment_tacker.insfrastructure.persistence;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "candidates")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CandidateEntity {
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private int experience;
+    private String appliedRole;
+    private double expectedSalary;
+    private enum status{
+        Applied,
+        Shortlisted,
+        Hired,
+        Rejected
+    }
+    private boolean isDeleted;
+
 }
