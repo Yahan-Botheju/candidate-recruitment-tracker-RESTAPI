bef0e856db3b07caa816859a548d7e008940d6f4
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-06 20:16:31 +0530

    feat: initiate prime key, add filed validations and separate the enum

--- src/main/java/lk/practice/candidate_recruitment_tacker/insfrastructure/persistence/CandidateEntity.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/insfrastructure/persistence/CandidateEntity.java
@@ -1,7 +1,11 @@
 package lk.practice.candidate_recruitment_tacker.insfrastructure.persistence;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lk.practice.candidate_recruitment_tacker.domain.model.CandidateStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,18 +17,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Full name is required")
     private String fullName;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @Min(value = 0, message = "Experience cannot be zero")
     private int experience;
+
+    @NotBlank(message = "Applied role cannot be empty")
     private String appliedRole;
+
+    @Positive(message = "Expected salary should be higher than zero")
     private double expectedSalary;
-    private enum status{
-        Applied,
-        Shortlisted,
-        Hired,
-        Rejected
-    }
-    private boolean isDeleted;
+
+    @Enumerated(EnumType.STRING)
+    private CandidateStatus status;
+
+    private boolean isDeleted = false;
 
 }
