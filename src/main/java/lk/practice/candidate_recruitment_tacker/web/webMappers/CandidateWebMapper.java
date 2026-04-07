7223392fb33a600783b8de5a3bfed0665b16d1d7
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 14:11:06 +0530

    fix: skip fields, request dto does not have those fields

--- src/main/java/lk/practice/candidate_recruitment_tacker/web/webMappers/CandidateWebMapper.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/web/webMappers/CandidateWebMapper.java
@@ -4,11 +4,14 @@ import lk.practice.candidate_recruitment_tacker.domain.model.Candidate;
 import lk.practice.candidate_recruitment_tacker.web.DTOs.CandidateRequestDTO;
 import lk.practice.candidate_recruitment_tacker.web.DTOs.CandidateResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CandidateWebMapper {
 
     //request DTO to domain model
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
     Candidate toDomainModel(CandidateRequestDTO candidateRequestDTO);
 
     //domain model response DTO
