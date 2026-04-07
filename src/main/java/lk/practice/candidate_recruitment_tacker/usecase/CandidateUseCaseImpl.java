89a41ab9eae7e316db90fa9cf7cb9b02f94d69c1
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 19:53:19 +0530

    feat: call the domain repo method for get the values

--- src/main/java/lk/practice/candidate_recruitment_tacker/usecase/CandidateUseCaseImpl.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/usecase/CandidateUseCaseImpl.java
@@ -1,11 +1,20 @@
 package lk.practice.candidate_recruitment_tacker.usecase;
 
+import lk.practice.candidate_recruitment_tacker.domain.model.Candidate;
 import lk.practice.candidate_recruitment_tacker.domain.repository.CandidateRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class CandidateUseCaseImpl implements CandidateUseCase{
 
     //inject domain repo for initiate class as bean
     private final CandidateRepository candidateRepository;
+
+    //get all candidates
+    @Override
+    public List<Candidate> getAllCandidates(){
+        return candidateRepository.getAllCandidates();
+    }
 }
