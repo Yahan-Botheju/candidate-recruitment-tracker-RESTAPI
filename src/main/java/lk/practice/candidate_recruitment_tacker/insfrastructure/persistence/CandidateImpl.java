23fcb5e69375bb13a9bdc20fe708a8e33ff2709a
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 19:02:56 +0530

    feat: inject jpa repo and persistence mapper

--- src/main/java/lk/practice/candidate_recruitment_tacker/insfrastructure/persistence/CandidateImpl.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/insfrastructure/persistence/CandidateImpl.java
@@ -1,4 +1,17 @@
 package lk.practice.candidate_recruitment_tacker.insfrastructure.persistence;
 
-public class CandidateImpl {
+import lk.practice.candidate_recruitment_tacker.domain.repository.CandidateRepository;
+import lk.practice.candidate_recruitment_tacker.insfrastructure.persistence.persistenceMappers.CandidatePersistenceMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CandidateImpl implements CandidateRepository {
+
+    //inject jpa repo
+    private final JpaCandidateRepository jpaCandidateRepository;
+
+    //inject persistence mapper
+    private final CandidatePersistenceMapper candidatePersistenceMapper;
 }
