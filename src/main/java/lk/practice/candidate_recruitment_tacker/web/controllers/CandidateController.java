d35cd32f190b46cccf878a5e6d1e09089971e4aa
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 20:36:48 +0530

    feat: inject usecase and web mapper

--- src/main/java/lk/practice/candidate_recruitment_tacker/web/controllers/CandidateController.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/web/controllers/CandidateController.java
@@ -1,4 +1,19 @@
 package lk.practice.candidate_recruitment_tacker.web.controllers;
 
+import lk.practice.candidate_recruitment_tacker.usecase.CandidateUseCase;
+import lk.practice.candidate_recruitment_tacker.web.webMappers.CandidateWebMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/candidates")
+@RequiredArgsConstructor
 public class CandidateController {
+
+    //inject candidate usecase
+    private final CandidateUseCase candidateUseCase;
+
+    //inject web mapper
+    private final CandidateWebMapper candidateWebMapper;
 }
