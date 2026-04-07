b4ef854e25e2caed8141f485b318b5a4b4b0f76f
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 10:13:05 +0530

    feat: define domain model

--- src/main/java/lk/practice/candidate_recruitment_tacker/domain/model/Candidate.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/domain/model/Candidate.java
@@ -1,4 +1,20 @@
 package lk.practice.candidate_recruitment_tacker.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Candidate {
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private int experience;
+    private String appliedRole;
+    private double expectedSalary;
+    private CandidateStatus status;
+    private boolean isDeleted = false;
 }
