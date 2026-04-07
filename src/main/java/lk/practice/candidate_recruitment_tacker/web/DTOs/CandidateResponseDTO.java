d30fd75fa0bbcc80f90fa9c73166174728ccbe4f
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 11:17:48 +0530

    feat: implement candidate response DTO

--- src/main/java/lk/practice/candidate_recruitment_tacker/web/DTOs/CandidateResponseDTO.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/web/DTOs/CandidateResponseDTO.java
@@ -1,4 +1,20 @@
 package lk.practice.candidate_recruitment_tacker.web.DTOs;
 
+import lk.practice.candidate_recruitment_tacker.domain.model.CandidateStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CandidateResponseDTO {
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private int experience;
+    private String appliedRole;
+    private double expectedSalary;
+    private CandidateStatus status;
 }
