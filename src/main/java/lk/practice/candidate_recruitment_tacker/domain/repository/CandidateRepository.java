caadf1212dd838c3a81dc5c37df626735a27c366
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 19:20:48 +0530

    feat: implement get all candidate method

--- src/main/java/lk/practice/candidate_recruitment_tacker/domain/repository/CandidateRepository.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/domain/repository/CandidateRepository.java
@@ -1,4 +1,12 @@
 package lk.practice.candidate_recruitment_tacker.domain.repository;
 
+import lk.practice.candidate_recruitment_tacker.domain.model.Candidate;
+import lk.practice.candidate_recruitment_tacker.insfrastructure.persistence.CandidateEntity;
+
+import java.util.List;
+
 public interface CandidateRepository {
+
+    //get all candidates
+    List<Candidate> getAllCandidates();
 }
