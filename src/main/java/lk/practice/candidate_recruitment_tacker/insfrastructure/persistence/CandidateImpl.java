896d4bb8ab8bc386cc2608d56c82d75243b06bd4
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 19:35:04 +0530

    feat: implement get all candidate data, turn into domain model through the mapper

--- src/main/java/lk/practice/candidate_recruitment_tacker/insfrastructure/persistence/CandidateImpl.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/insfrastructure/persistence/CandidateImpl.java
@@ -1,9 +1,12 @@
 package lk.practice.candidate_recruitment_tacker.insfrastructure.persistence;
 
+import lk.practice.candidate_recruitment_tacker.domain.model.Candidate;
 import lk.practice.candidate_recruitment_tacker.domain.repository.CandidateRepository;
 import lk.practice.candidate_recruitment_tacker.insfrastructure.persistence.persistenceMappers.CandidatePersistenceMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,4 +17,15 @@ public class CandidateImpl implements CandidateRepository {
 
     //inject persistence mapper
     private final CandidatePersistenceMapper candidatePersistenceMapper;
+
+    //get all candidates
+    @Override
+    public List<Candidate> getAllCandidates(){
+        //get all candidates
+        List<CandidateEntity> candidateEntities = jpaCandidateRepository.findAll();
+
+        //return as domain model candidates list
+        return candidateEntities.stream()
+                .map(candidatePersistenceMapper::toDomainModel).toList();
+    }
 }
