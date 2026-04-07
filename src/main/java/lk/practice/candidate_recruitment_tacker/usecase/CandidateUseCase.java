7a0852cd675ca1861cd191aed7c167cf27cce72a
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 19:51:38 +0530

    feat: initiate get all candidate method in usecase interface

--- src/main/java/lk/practice/candidate_recruitment_tacker/usecase/CandidateUseCase.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/usecase/CandidateUseCase.java
@@ -1,4 +1,11 @@
 package lk.practice.candidate_recruitment_tacker.usecase;
 
+import lk.practice.candidate_recruitment_tacker.domain.model.Candidate;
+
+import java.util.List;
+
 public interface CandidateUseCase {
+
+    //get all candidates
+    List<Candidate> getAllCandidates();
 }
