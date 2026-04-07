c6607c1f2c59ce458da7fc6f97b3353d3c96dc0e
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-07 10:51:47 +0530

    feat: add mapstructure library dependency and implement web mapper

--- src/main/java/lk/practice/candidate_recruitment_tacker/web/webMappers/CandidateWebMapper.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/web/webMappers/CandidateWebMapper.java
@@ -1,4 +1,16 @@
 package lk.practice.candidate_recruitment_tacker.web.webMappers;
 
+import lk.practice.candidate_recruitment_tacker.domain.model.Candidate;
+import lk.practice.candidate_recruitment_tacker.web.DTOs.CandidateRequestDTO;
+import lk.practice.candidate_recruitment_tacker.web.DTOs.CandidateResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface CandidateWebMapper {
+
+    //request DTO to domain model
+    Candidate toDomainModel(CandidateRequestDTO candidateRequestDTO);
+
+    //domain model response DTO
+    CandidateResponseDTO toResponseDTO(Candidate candidate);
 }
