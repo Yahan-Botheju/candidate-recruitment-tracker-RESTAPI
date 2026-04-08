66d75fa5d05fc5ef75dca0b3956a6e98be49b7b8
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-08 17:59:52 +0530

    feat: create get all candidate route/controller/web

--- src/main/java/lk/practice/candidate_recruitment_tacker/web/controllers/CandidateController.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/web/controllers/CandidateController.java
@@ -1,10 +1,15 @@
 package lk.practice.candidate_recruitment_tacker.web.controllers;
 
 import lk.practice.candidate_recruitment_tacker.usecase.CandidateUseCase;
+import lk.practice.candidate_recruitment_tacker.web.DTOs.CandidateResponseDTO;
 import lk.practice.candidate_recruitment_tacker.web.webMappers.CandidateWebMapper;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/candidates")
@@ -16,4 +21,14 @@ public class CandidateController {
 
     //inject web mapper
     private final CandidateWebMapper candidateWebMapper;
+
+    //get all candidates
+    @GetMapping("/all")
+    public List<CandidateResponseDTO> getAllCandidates(){
+
+        //get domain model through usecase, through web mapper turn that into response dto and return
+        return candidateUseCase.getAllCandidates()
+                .stream()
+                .map(candidateWebMapper::toResponseDTO).toList();
+    }
 }
