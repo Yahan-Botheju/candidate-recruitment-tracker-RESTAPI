398ed9243a1a154ff3a512dc4ef9e120d0d35b59
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-09 19:30:40 +0530

    feat: initiate delete method in usecase interface

--- src/main/java/lk/practice/candidate_recruitment_tacker/usecase/CandidateUseCase.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/usecase/CandidateUseCase.java
@@ -14,4 +14,7 @@ public interface CandidateUseCase {
 
     //update candidate
     void updateCandidate(Long id, Candidate candidate);
+
+    //delete candidate
+    void deleteCandidate(Long id);
 }
