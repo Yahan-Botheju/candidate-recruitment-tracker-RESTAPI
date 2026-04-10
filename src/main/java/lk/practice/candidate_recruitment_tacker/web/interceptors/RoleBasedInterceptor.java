896ebed388983b36d17261a411a6ef4fd082b4fd
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-10 20:41:36 +0530

    feat: implement interceptor functionality

--- src/main/java/lk/practice/candidate_recruitment_tacker/web/interceptors/RoleBasedInterceptor.java
+++ src/main/java/lk/practice/candidate_recruitment_tacker/web/interceptors/RoleBasedInterceptor.java
@@ -1,4 +1,37 @@
 package lk.practice.candidate_recruitment_tacker.web.interceptors;
 
-public class RoleBasedInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class RoleBasedInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull Object handler
+    ) throws Exception{
+
+        //get req method
+        String getMethod = request.getMethod();
+
+        //take role from header
+        String role = request.getHeader("X-User-Role");
+
+        //req delete and role != ADMIN then block
+        if("DELETE".equalsIgnoreCase(getMethod)){
+            if(role == null || role.equalsIgnoreCase("ADMIN")){
+              //throw 403 error
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.getWriter().write("Access denied: Only ADMIN can delete candidates");
+
+                return false;
+            }
+        }
+        return true;
+    }
 }
