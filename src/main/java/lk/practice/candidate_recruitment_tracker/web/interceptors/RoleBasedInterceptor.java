aab8ccc60d466f4fe0a8c7e95fd7ac18c247758e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-13 22:26:19 +0530

    fix: admin only can update status of candidate function check through interceptor

--- src/main/java/lk/practice/candidate_recruitment_tracker/web/interceptors/RoleBasedInterceptor.java
+++ src/main/java/lk/practice/candidate_recruitment_tracker/web/interceptors/RoleBasedInterceptor.java
@@ -19,9 +19,15 @@ public class RoleBasedInterceptor implements HandlerInterceptor {
         //get req method
         String reqMethod = request.getMethod();
 
+        //get req uri
+        String uri = request.getRequestURI();
+
         //take role from header
         String role = request.getHeader("X-User-Role");
 
+
+
+         /* DELETE */
         //req delete and role != ADMIN then block
         if("DELETE".equalsIgnoreCase(reqMethod)){
             if(role == null || ! role.trim().equalsIgnoreCase("ADMIN")){
@@ -32,6 +38,20 @@ public class RoleBasedInterceptor implements HandlerInterceptor {
                 return false;
             }
         }
+
+        /* STATUS CHANGE */
+        //only ADMIN can change status, if not throw and error
+        if("PUT".equalsIgnoreCase(reqMethod) && uri.startsWith("/api/v1/candidates/status/")){
+            if (role == null || ! role.trim().equalsIgnoreCase("ADMIN")) {
+                //throw 403 error
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.getWriter().write("Access denied: Only ADMIN can update candidate statuses");
+
+                return false;
+            }
+        }
+
+
         return true;
     }
 }
